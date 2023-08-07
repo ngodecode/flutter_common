6af719d919ed0661710134d4abbcd7aab77cccd9
febriarianto <febri.arianto@amartha.com>
2023-08-07 21:01:42 +0700

    fix export

--- lib/src/commons/common_router/lib/common_router.dart
+++
@@ -1,4 +0,0 @@
-export 'package:common_router/src/route/common_route_navigator.dart';
-export 'package:common_router/src/route/common_route.dart';
-export 'package:common_router/src/route/common_route_registrar.dart';
-export 'package:common_router/src/extension/url_modifier.dart';
